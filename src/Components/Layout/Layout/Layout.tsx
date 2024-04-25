4823ad09ffc563bd72b1273d14433787bdd9a3d1
Tsuri <tsuri.he@gmail.com>
2024-04-25 15:36:08 +0300

    firstcommit

---
+++ src/Components/Layout/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import "./Layout.css";
+
+export function Layout(): JSX.Element {
+    return (
+        <div className="Layout">
+
+            <header>
+
+            </header>
+
+            <main>
+
+            </main>
+
+            <footer>
+
+            </footer>
+        </div>
+    );
+}
