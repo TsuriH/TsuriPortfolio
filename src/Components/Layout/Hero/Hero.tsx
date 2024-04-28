d8096bdbbe257b42821f543833a256fa454bbad3
TsuriHefer <tsuri.he@gmail.com>
2024-04-28 16:55:45 +0300

    finish hero

--- src/Components/Layout/Hero/Hero.tsx
+++ src/Components/Layout/Hero/Hero.tsx
@@ -1,14 +1,22 @@
 import "./Hero.css";
-
+import { NavLink } from "react-router-dom";
 export function Hero(): JSX.Element {
     return (
         <div className="Hero">
-            <h2>Hi!</h2>
-            <p className="self-description">
-                I'm A Full Stack <span>Web Developer</span></p>
-            <p className="about">
-            Coming from the motion design field gives me a better understanding of design principles and a keen eye for aesthetics, which I blend with my technical skills on every project
-            </p>
+            <div className="hero-container">
+                <h2>Hi, my name is Tsuri</h2>
+                <p className="self-description">
+                    I'm A Full Stack <br></br><span>Web Developer</span></p>
+                <p className="about">
+                    {/* Coming from the motion design field gives me a better understanding of design principles and a keen eye for aesthetics, which I blend with my technical skills on every project */}
+                </p>
+                <div className="nav-buttons">
+                    <NavLink to="project" className="nav-btn">Project</NavLink>
+                    <NavLink to="about" className="nav-btn">About</NavLink>
+                    <NavLink to="contact" className="nav-btn">Contact</NavLink>
+                </div>
+            </div>
+
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { Layout } from './Components/Layout/Layout/Layout';
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Layout />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Layout />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
