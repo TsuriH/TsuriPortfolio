918d151e9b17c76e29d7a92835c71bce7c8fa7fc
Tsuri <tsuri.he@gmail.com>
2024-05-20 08:35:03 +0300

    about

--- src/Components/Layout/About/About.tsx
+++ src/Components/Layout/About/About.tsx
@@ -3,7 +3,15 @@ import "./About.css";
 function About(): JSX.Element {
     return (
         <div className="About">
-			<p>I'm the about component</p>
+            <h1>about me.</h1>
+            <p className="welcome desc">Hello my name is <span>Tsuri.</span> </p>
+            <p className="desc">Welcome to my portfolio.</p>
+            <p className="first-paragraph desc">I’m <span>Full Stack Web Developer </span> passionate about Front-End, particularly <span>React.</span></p>
+            <p className="second-paragraph desc">Having saying that I do <span>backend</span> too.</p>
+            <p className="third-paragraph desc">Since I can remember, I have always been drawn to visuals and graphics, especially interactive ones.</p>
+            <p className="forth-paragraph desc">I have a background in <span>motion design</span> and <span>graphic design</span>, in addition to expertise in <span>UX/UI</span>, client relations, and creativity.</p>.
+
+
         </div>
     );
 }
